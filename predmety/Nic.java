71fcb6f3f8dbb359670ff90ec8a68abd0d37521f
Hajadlo <Hajadlo@gmail.com>
2015-09-08 09:27:26 +0200

    1st commit

---
+++ predmety/Nic.java
@@ -0,0 +1,15 @@
+package predmety;
+
+public class Nic extends Predmet{
+
+	public Nic(String nazev) {
+		super(nazev);
+		this.proPovolani = "Oboje";
+		this.utok = 0;
+		this.obrana = 0;
+		this.bonusStr = 0;
+		this.bonusInt = 0;
+		this.cena = 0;
+	}
+
+}
